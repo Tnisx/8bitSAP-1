7abe378de302815b9ce42b1cb2cf9097c293e664
Tnisx <tnisx18@gmail.com>
2026-06-09 18:26:55 +0530

    Fix comment formatting in IR.v module

--- src/IR.v
+++ src/IR.v
@@ -2,8 +2,8 @@ module ir (
     input        clk,
     input        load,          // from CU
     input  [7:0] data_in,       // from bus
-    output reg [3:0] opcode,        // upper 4 bits → to CU
-    output reg [3:0] operand        // lower 4 bits → to MAR
+    output reg [3:0] opcode,        // upper 4 bits to to CU
+    output reg [3:0] operand        // lower 4 bits to to MAR
 );
 always @(posedge clk)begin
 if(load)begin
